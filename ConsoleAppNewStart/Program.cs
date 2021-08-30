b678f0e1ce39d91f7633166dacb5b157c3a1b358
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-08-30 13:56:38 +0200

    method and string to int convet added

--- ConsoleAppNewStart/Program.cs
+++ ConsoleAppNewStart/Program.cs
@@ -4,10 +4,42 @@ namespace ConsoleAppNewStart
 {
     class Program
     {
+        /*
+        block comment
+        many lines
+        */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("I love to code");
+
+            string userAgeString = AskUserFor("your age");
+
+            Console.WriteLine("You type in " + userAgeString + " as you age.");
+
+            int userAge = Convert.ToInt32(userAgeString);
+
+            if (userAge >= 18)
+            {
+                Console.WriteLine("Welcome to the bar and have a beer");
+            }
+            else
+            {
+                Console.WriteLine("Welcome to the candy store, want a lollipop?");
+            }
+
+
+        }//end of Main method
+
+//      modifire - return type - name - parameter inputs
+        static string AskUserFor(string what)
+        {
+            Console.Write("Please enter " + what + ": ");
+
+            string userInput = Console.ReadLine();
+
+            return userInput;
         }
-    }
-}
+
+    }//end of program class
+}//end of namespace
