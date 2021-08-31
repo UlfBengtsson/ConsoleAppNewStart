eedeae7d4e6e1cb1fbf2a4b50ef9dd7746c70436
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-08-31 10:01:54 +0200

    menu added

--- ConsoleAppNewStart/Program.cs
+++ ConsoleAppNewStart/Program.cs
@@ -10,14 +10,50 @@ namespace ConsoleAppNewStart
         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("I love to code");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("I love to code");
 
+            bool keepLooping = true;
+
+            while (keepLooping)
+            {
+                Console.Clear();
+                Console.WriteLine("---- Menu ----\n1. Age limited Store´s\n9. Exit\n");
+
+                int selection = AskUserForNumber("your selection");
+
+                switch (selection)
+                {
+                    case 1:
+                        AgeLimitStors();
+                        break;
+
+                    case 9:
+                        keepLooping = false;
+                        Console.WriteLine("Thanks for youring this program.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Incorrect selection!");
+                        break;
+                }
+                PressToContinue();
+            }
+
+
+        }//end of Main method
+
+        static void AgeLimitStors()
+        {
+            /*
             string userAgeString = AskUserFor("your age");
 
             Console.WriteLine("You type in " + userAgeString + " as you age.");
 
             int userAge = Convert.ToInt32(userAgeString);
+            */
+
+            int userAge = AskUserForNumber("your age");
 
             if (userAge >= 18)
             {
@@ -28,8 +64,7 @@ namespace ConsoleAppNewStart
                 Console.WriteLine("Welcome to the candy store, want a lollipop?");
             }
 
-
-        }//end of Main method
+        }
 
 //      modifire - return type - name - parameter inputs
         static string AskUserFor(string what)
@@ -39,6 +74,25 @@ namespace ConsoleAppNewStart
             string userInput = Console.ReadLine();
 
             return userInput;
+        }
+
+        static int AskUserForNumber(string what)
+        {
+            bool wasNotNumber = true;
+            int number = 0;
+            do
+            {
+                wasNotNumber = ! int.TryParse(AskUserFor(what), out number);
+
+            } while (wasNotNumber);
+
+            return number;
+        }
+
+        static void PressToContinue()
+        {
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
         }
 
     }//end of program class
